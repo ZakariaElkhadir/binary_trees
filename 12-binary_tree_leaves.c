fbbda8bf4f0f16b37f1382eeb99254235875c702
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-01 20:54:57 +0100

    betty fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -16,7 +16,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (!tree->left && !tree->right)
 		return (1);
-	
+
 
 	rleave = binary_tree_leaves(tree->right);
 	lleave = binary_tree_leaves(tree->left);
