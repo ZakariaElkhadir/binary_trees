c4e730495f6de1329eefd41587ac0d78a8b7b528
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-03 00:49:43 +0100

    add fiel

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - find the sibling of a node
+ * @node: node to find sibling of
+ * Return: pointer to sibling if not NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node->parent->left != NULL && node->parent->left == node)
+		return (node->parent->right);
+
+	else if (node->parent->right != NULL && node->parent->right == node)
+		return (node->parent->left);
+
+	return (NULL);
+}
