197834cf66a1664054e11f4f2a2609322ac9ab0d
ZakariaElkhadir <zelkhadir5@gmail.com>
2024-02-02 22:20:24 +0100

    add file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,10 +1,38 @@
 #include "binary_trees.h"
 
+int the_depth(const binary_tree_t *tree)
+{
+  int d = 0;
+  while (tree != NULL)
+  {
+    d++;
+    tree = tree->left;
+  }
+  return (d);
+}
+
+int is_perfect(const binary_tree_t *tree, int dpth, int level)
+{
+  if (!tree)
+    return (1);
+
+  if (!tree->left && !tree->right)
+    return (dpth == (level + 1));
+
+  if (!tree->left || !tree->right)
+		return (0);
+
+   return (is_perfect(tree->left, dpth, level + 1) &&
+			is_perfect(tree->right, dpth, level + 1));
+}
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-  if (tree == NULL)
+  int depth;
+
+  if (!tree)
     return (0);
 
-  if (tree->left == NULL && tree->right == NULL)
-
+  depth = the_depth(tree);
+  return (is_perfect(tree, depth, 0));
 }
